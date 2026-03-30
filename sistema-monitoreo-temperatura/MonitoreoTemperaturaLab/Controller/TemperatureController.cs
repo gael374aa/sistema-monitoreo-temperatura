4b4bc218e084b16fd7ff95509efb22b37d1fdef5
AGUSTIN GAEL GRIJALVA MARTINEZ <L242T0453@misantla.tecnm.mx>
2026-03-29 18:48:47 -0600

    preparacion de entorno

---
+++ sistema-monitoreo-temperatura/MonitoreoTemperaturaLab/Controller/TemperatureController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using MonitoreoTemp.Models;
+
+namespace MonitoreoTemp.Controllers
+{
+    public class TemperatureController
+    {
+        private bool _ejecutando = false;
+        public delegate void OnTempChanged(SensorData data);
+        public event OnTempChanged TempChanged; // Evento para avisar a la Vista
+
+        public void IniciarSimulacion()
+        {
+            _ejecutando = true;
+            Thread hiloSimulador = new Thread(Simular);
+            hiloSimulador.Start();
+        }
+
+        private void Simular()
+        {
+            Random r = new Random();
+            while (_ejecutando)
+            {
+                var data = new SensorData
+                {
+                    Valor = r.NextDouble() * (35 - 15) + 15, // Entre 15 y 35 grados
+                    FechaHora = DateTime.Now
+                };
+
+                // Notificar a la Vista
+                TempChanged?.Invoke(data);
+
+                Thread.Sleep(2000); // Pausa de 2 segundos
+            }
+        }
+    }
+}
---
+++ sistema-monitoreo-temperatura/MonitoreoTemperaturaLab/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MonitoreoTemp.Models; // Para que reconozca SensorData
+using MonitoreoTemp.Controllers; // Para que reconozca el Controlador
+
+namespace MonitoreoTemp.Views
+{
+    public partial class Form1 : Form
+    {
+        // 1. Instanciamos el controlador (el motor)
+        private TemperatureController _controller = new TemperatureController();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 2. Nos suscribimos al evento: "Avisame cuando cambie la temp"
+            _controller.TempChanged += AlRecibirNuevaTemperatura;
+
+            // 3. Arrancamos el motor
+            _controller.IniciarSimulacion();
+        }
+
+        // 4. ESTA ES LA FUNCIÓN DEL PASO 4 (El puente seguro)
+        private void AlRecibirNuevaTemperatura(SensorData data)
+        {
+            // ¿Estoy en el hilo equivocado?
+            if (this.InvokeRequired)
+            {
+                // Si sí, me mando llamar a mí mismo pero en el hilo correcto
+                this.Invoke(new Action(() => AlRecibirNuevaTemperatura(data)));
+                return;
+            }
+
+            // --- AQUÍ TRABAJAN TUS COMPAÑEROS ---
+            // Todo lo que escriban aquí abajo es SEGURO y no romperá el programa
+
+           
+
+            
+        }
+    }
+}
