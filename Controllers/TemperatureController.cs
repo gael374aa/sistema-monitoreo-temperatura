a9c96173135733a343332615a2eebbebe3225932
gael374aa <gm918098@gmail.com>
2026-04-17 10:29:19 -0600

    Add files via upload

---
+++ Controllers/TemperatureController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using MonitoreoTemperaturaLab.Models;
+
+namespace MonitoreoTemperaturaLab.Controllers
+{
+    public class TemperatureController
+    {
+        private bool _ejecutando = false;
+        public delegate void OnTempChanged(SensorData data);
+        public event OnTempChanged TempChanged;
+
+        public void IniciarSimulacion()
+        {
+            if (_ejecutando) return; // Evita que se inicien varios hilos
+            _ejecutando = true;
+            Thread hiloSimulador = new Thread(Simular);
+            hiloSimulador.IsBackground = true; // Para que se cierre al cerrar la app
+            hiloSimulador.Start();
+        }
+        public void DetenerSimulacion()
+        {
+            _ejecutando = false; // Esto hará que el bucle while en el hilo se detenga
+        }
+
+        private void Simular()
+        {
+            Random r = new Random();
+            while (_ejecutando)
+            {
+                var data = new SensorData
+                {
+                    Valor = r.NextDouble() * (35 - 15) + 15, // Simula entre 15 y 35 grados
+                    FechaHora = DateTime.Now
+                };
+
+                TempChanged?.Invoke(data);
+                Thread.Sleep(2000); // Actualiza cada 2 segundos
+            }
+        }
+    }
+}
